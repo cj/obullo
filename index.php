8baa09cca56567597f135e53265239213e6f5a23
Ersin Güvenç <eguvenc@gmail.com>
2009-09-27 19:48:42 +0000

    added $GLOBALS['d'] for directory fetch support changed routing as /directory/class/method/arguments... array_slice($Uri->rsegments, 2) changed as array_slice($Uri->rsegments, 3)

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+/* SVN FILE: $Id: index.php $Rev: 15 27-09-2009 21:25 develturk $ */
+
 // This function just for memory testing..
 function OB_memory_usage() {
 $usage = '';
@@ -37,10 +39,19 @@ $start = memory_get_usage();
                               
 /* ---------------------------------------------------------------------------*/                               
 
-
+/**
+* Index.php
+* 
+* @version 1.0
+* @version 1.1 added router and uri classes
+* @version 1.2 added router and uri classes
+* @version 1.3 changed directory structure completely
+*              like /directory/class/method/arguments...
+*/
 
 header('Content-type: text/html;charset=UTF-8'); 
 
+// Our Top Common Exception Class
 Class CommonException extends Exception {}  
 
 // Base paths
@@ -53,25 +64,29 @@ define('VIEW', 'application'.DS.'controllers'.DS);
 define('CONTROLLER', 'application'.DS.'controllers'.DS);
 define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));  
 
+require (BASE.'libraries'.DS.'Errors'.EXT); 
 require (BASE.'libraries'.DS.'Registry'.EXT); 
 require (BASE.'libraries'.DS.'Library_factory'.EXT); 
 require (BASE.'Common'.EXT);    
-require (BASE.'libraries'.DS.'Errors'.EXT); 
+
 
 $Config = base_register('Config');
 $Uri    = base_register('URI');
 $Router = base_register('Router');
 
+//echo 'directory: '.$Router->fetch_directory().'<br />'; 
 //echo 'class: '.$Router->fetch_class().'<br />';
-//echo 'method: '.$Router->fetch_method();  exit;
+//echo 'method: '.$Router->fetch_method(); 
 
+// directory    
+$GLOBALS['d']   = $Router->fetch_directory();  
 $GLOBALS['c']   = $Router->fetch_class();  // Get requested controller
 $GLOBALS['m']   = $Router->fetch_method(); // Get requested method
 
 // Check the controller exists or not
-if ( ! file_exists(APP.'controllers'.DS.$GLOBALS['c'].DS.$GLOBALS['c'].EXT))
+if ( ! file_exists(APP.'controllers'.DS.$GLOBALS['d'].DS.$GLOBALS['c'].EXT))
 {
-    new CommonException('Unable to load your default controller.
+    throw new CommonException('Unable to load your default controller.
     Please make sure the controller specified in your Routes.php file is valid.');
 }
 
@@ -82,7 +97,7 @@ require (BASE.'libraries'.DS.'Library'.EXT);
 require (BASE.'libraries'.DS.'Model'.EXT);
 
 // call the controller.
-require (CONTROLLER.$GLOBALS['c'].DS.$GLOBALS['c'].EXT);
+require (CONTROLLER.$GLOBALS['d'].DS.$GLOBALS['c'].EXT);
 
 
 if ( ! class_exists($GLOBALS['c'])
@@ -105,9 +120,11 @@ if ( ! in_array(strtolower($GLOBALS['m']), array_map('strtolower', get_class_met
     show_404("{$GLOBALS['c']}/{$GLOBALS['m']}"); 
 }
 
+//print_r($Uri->rsegments);
+
 // Call the requested method.
-// Any URI segments present (besides the class/function) will be passed to the method for convenience
-call_user_func_array(array($OB, $GLOBALS['m']), array_slice($Uri->rsegments, 2));
+// Any URI segments present (besides the directory/class/function) will be passed to the method for convenience
+call_user_func_array(array($OB, $GLOBALS['m']), array_slice($Uri->rsegments, 3));
 
 
 //$end = OB_memory_usage();
