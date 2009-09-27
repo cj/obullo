eae2f4a498190ed2f3abbd52fb09fa180723b216
Ersin Güvenç <eguvenc@gmail.com>
2009-09-27 19:56:11 +0000

    addded directory trigger comment

--- application/config/config.php
+++ application/config/config.php
@@ -151,7 +151,7 @@ $config['permitted_uri_chars']    = 'a-z 0-9~%.:_\-';
 $config['enable_query_strings']   = FALSE;
 $config['controller_trigger']     = 'c';
 $config['function_trigger']       = 'm';
-$config['directory_trigger']      = 'd'; // experimental not currently in use
+$config['directory_trigger']      = 'd'; // we currently use it in Obullo :)
 
 /*
 |--------------------------------------------------------------------------
