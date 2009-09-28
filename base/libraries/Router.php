b8c315cca4a94fcff609a2aef21ce2c1db84a882
Ersin Güvenç <eguvenc@gmail.com>
2009-09-28 08:49:42 +0000

    html mistake fixed :=)

--- base/libraries/Router.php
+++ base/libraries/Router.php
@@ -408,17 +408,3 @@ class OB_Router {
 /* Location: ./system/libraries/Router.php */
 
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
-
-<html>
-<head>
-  <meta name="generator" content=
-  "HTML Tidy for Windows (vers 14 February 2006), see www.w3.org">
-
-  <title></title>
-</head>
-
-<body>
-</body>
-</html>
