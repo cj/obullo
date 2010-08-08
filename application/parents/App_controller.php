16f7471ef1a3f74e4e4d15803fc668167c108020
Ersin Güvenç <eguvenc@gmail.com>
2010-08-08 12:05:22 +0000

    comments removed

--- application/parents/App_controller.php
+++ application/parents/App_controller.php
@@ -1,18 +1,12 @@
 <?php
 defined('BASE') or exit('Access Denied!');
-                                  
-/**
-* Global Application Controller
-*/
+                                                     
 Class App_controller 
 {
     public $base, $base_url, $base_img;
     public $title, $head, $meta, $body;
     public $body_attributes = ''; 
     
-    /**
-    * Global Autoloader
-    */
     function __autoloader()
     {         
         $this->config->auto_base_url(true);
