3da7015183974c220ea8714c9eb281e0e25e684e
Ersin Güvenç <eguvenc@gmail.com>
2010-10-19 20:17:05 +0000

    config/parents.php fixed some comments change_log.txt

--- application/config/parents.php
+++ application/config/parents.php
@@ -2,24 +2,11 @@
 defined('BASE') or exit('Access Denied!'); 
 /*
 | -------------------------------------------------------------------------
-| USER DEFINED PARENT CONTROLLERS
+| USER DEFINED GLOBAL CONTROLLERS (GC)
 | -------------------------------------------------------------------------
-| This file 
-|
 | Please see the user guide for complete details:
 |
 | Chapters / General Topics / Global Controllers
-|
-| -------------------------------------------------------------------------
-| RESERVED PARENT NAMES
-| -------------------------------------------------------------------------
-| Global Controller is your DEFAULT parent controller. All controllers 
-| should be parent of the Global Controller except the your 
-| special controllers like rest controller etc ..
-|
-|  Reserved Name          Reserved Parent Name
-|  o Global_Controller    parent::__global();
-|  o Core_Controller
 |
 */
 
@@ -29,9 +16,10 @@ defined('BASE') or exit('Access Denied!');
 |--------------------------------------------------------------------------
 | You can assign Global Controllers to Folders.
 |
-| Forexample 
-| If router requests "index.php/rest/" match with directory
-| regex (rest) then the "all" Controllers which is located in rest folder 
+| Description:
+|
+| If there is any regex match like "index.php/rest/" with directory
+| then the "all" Controllers which is located in rest folder 
 | will extend to Global Rest_controller .
 |
 */
@@ -45,26 +33,25 @@ $parents['directory']['rest']       = 'Rest_controller';
 | To create the parent controller you must define controller
 | name here. 
 |
-| Description:
- 
-| If router requests "index.php/members/signup_form" match with controller
-| regex (_form$) then the "all" Controllers which has subrefix "_form" will 
-| extend to Global Form_controller .
-|
 | Warning : A Global Controller name writing style must be like
 | this "Foo_controller";
+|
+| Description:
+|
+| If router requests "index.php/members/signup_form" match with regex (_form$)
+| extend rules then the "all" Controllers which has suffix "_form" will 
+| extend to Global Form_controller .
 |
 | Examples:
 | 
 */
-// the controller name suffix which end of the "_foo" will extend to Global "Foo_controller" 
-$parents['controller']['(_foo$)']   =  'Foo_controller';
+// the controller name suffix which end of the "_foo" will extend to Global "Form_controller" 
+$parents['controller']['(_form$)']   =  'Form_controller';
 
 // the controller name prefix which start of the "bar_" will extend to Global "Bar_controller"
-$parents['controller']['(bar_*)']   =  'Bar_controller';
+$parents['controller']['(bar_*)']    =  'Bar_controller';
 
-                                     
-//-------------------------------------------------------------------------
+
 
 /* End of file parents.php */
 /* Location: .application/config/parents.php */
